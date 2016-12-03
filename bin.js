1f7fe89bff8258847a4a8aa59537fc50640b6c07
Vincent Weevers <mail@vincentweevers.nl>
2016-12-03 11:30:49 +0100

    feat: bin

---
+++ bin.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+'use strict';
+
+const vi = require('./')
+    , files = process.argv.slice(2)
+
+if (!files.length) {
+  console.error('usage: version-info <file>, ..')
+  process.exit(1)
+}
+
+console.log(JSON.stringify(files.map(f => vi(f)), null, 2))
