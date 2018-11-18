81d2d1b724e72a354ee15821a9eaa3904572e983
Vincent Weevers <mail@vincentweevers.nl>
2018-11-18 19:53:50 +0100

    test require() on non-windows

--- test/basic-test.js
+++ test/basic-test.js
@@ -1,126 +1,134 @@
 'use strict'
 
 const test = require('tape')
-const xtend = require('xtend')
-const gen = require('win-dummy-exe')
 const vi = require('..')
 
-var dummyDefaults = {
-  InternalName: 'dummy.exe',
-  OriginalFilename: 'dummy.exe'
-}
-
-test('company', function (t) {
-  t.plan(2)
-
-  gen({ assemblyCompany: 'beep' }, function (err, exe) {
-    t.ifError(err, 'no gen error')
-
-    t.same(vi(exe), xtend(dummyDefaults, {
-      CompanyName: 'beep',
-    }), 'info ok')
+if (process.platform !== 'win32') {
+  test('is a noop on ' + process.platform, function (t) {
+    t.same(vi('foo'), {})
+    t.end()
   })
-})
+} else {
+  const gen = require('win-dummy-exe')
+  const xtend = require('xtend')
 
-test('trims values', function (t) {
-  t.plan(2)
+  var dummyDefaults = {
+    InternalName: 'dummy.exe',
+    OriginalFilename: 'dummy.exe'
+  }
 
-  gen({ assemblyCompany: '  beep   ' }, function (err, exe) {
-    t.ifError(err, 'no gen error')
+  test('company', function (t) {
+    t.plan(2)
 
-    t.same(vi(exe), xtend(dummyDefaults, {
-      CompanyName: 'beep',
-    }), 'info ok')
-  })
-})
+    gen({ assemblyCompany: 'beep' }, function (err, exe) {
+      t.ifError(err, 'no gen error')
 
-test('ignores version 0', function (t) {
-  t.plan(4)
-
-  gen({ assemblyFileVersion: '0.0.0.0', assemblyInformationalVersion: '2.0' }, function (err, exe) {
-    t.ifError(err, 'no gen error (1)')
-
-    t.same(vi(exe), xtend(dummyDefaults, {
-      ProductVersion: '2.0'
-    }), 'info ok (1)')
+      t.same(vi(exe), xtend(dummyDefaults, {
+        CompanyName: 'beep',
+      }), 'info ok')
+    })
   })
 
-  gen({ assemblyFileVersion: '1.0.0.1', assemblyInformationalVersion: '0.0' }, function (err, exe) {
-    t.ifError(err, 'no gen error (1)')
+  test('trims values', function (t) {
+    t.plan(2)
 
-    t.same(vi(exe), xtend(dummyDefaults, {
-      FileVersion: '1.0.0.1'
-    }), 'info ok (1)')
-  })
-})
+    gen({ assemblyCompany: '  beep   ' }, function (err, exe) {
+      t.ifError(err, 'no gen error')
 
-test('versions', function (t) {
-  t.plan(2)
-
-  gen({ assemblyFileVersion: '2.0.0.2', assemblyInformationalVersion: '2.0b' }, function (err, exe) {
-    t.ifError(err, 'no gen error')
-
-    t.same(vi(exe), xtend(dummyDefaults, {
-      FileVersion: '2.0.0.2',
-      ProductVersion: '2.0b'
-    }), 'info ok')
-  })
-})
-
-test('utf-8', function (t) {
-  t.plan(6)
-
-  gen({ assemblyCopyright: '© Beep 嘟 Inc.' }, function (err, exe) {
-    t.ifError(err, 'no gen error')
-
-    t.same(vi(exe), xtend(dummyDefaults, {
-      LegalCopyright: '© Beep 嘟 Inc.'
-    }), 'info ok')
+      t.same(vi(exe), xtend(dummyDefaults, {
+        CompanyName: 'beep',
+      }), 'info ok')
+    })
   })
 
-  gen({ assemblyCompany: '   © Beep 嘟 Inc.  ' }, function (err, exe) {
-    t.ifError(err, 'no gen error')
+  test('ignores version 0', function (t) {
+    t.plan(4)
 
-    t.same(vi(exe), xtend(dummyDefaults, {
-      CompanyName: '© Beep 嘟 Inc.'
-    }), 'info ok')
+    gen({ assemblyFileVersion: '0.0.0.0', assemblyInformationalVersion: '2.0' }, function (err, exe) {
+      t.ifError(err, 'no gen error (1)')
+
+      t.same(vi(exe), xtend(dummyDefaults, {
+        ProductVersion: '2.0'
+      }), 'info ok (1)')
+    })
+
+    gen({ assemblyFileVersion: '1.0.0.1', assemblyInformationalVersion: '0.0' }, function (err, exe) {
+      t.ifError(err, 'no gen error (1)')
+
+      t.same(vi(exe), xtend(dummyDefaults, {
+        FileVersion: '1.0.0.1'
+      }), 'info ok (1)')
+    })
   })
 
-  gen({ assemblyTitle: '  嘟© Beep 嘟 Inc.嘟' }, function (err, exe) {
-    t.ifError(err, 'no gen error')
+  test('versions', function (t) {
+    t.plan(2)
 
-    t.same(vi(exe), xtend(dummyDefaults, {
-      FileDescription: '嘟© Beep 嘟 Inc.嘟'
-    }), 'info ok')
+    gen({ assemblyFileVersion: '2.0.0.2', assemblyInformationalVersion: '2.0b' }, function (err, exe) {
+      t.ifError(err, 'no gen error')
+
+      t.same(vi(exe), xtend(dummyDefaults, {
+        FileVersion: '2.0.0.2',
+        ProductVersion: '2.0b'
+      }), 'info ok')
+    })
   })
-})
 
-test('throws on invalid input', function (t) {
-  t.plan(4)
+  test('utf-8', function (t) {
+    t.plan(6)
 
-  throws('win-version-info requires a non-empty string filename', '')
-  throws('win-version-info is unable to access file: ' + process.cwd() + '\\nope.exe', 'nope.exe')
+    gen({ assemblyCopyright: '© Beep 嘟 Inc.' }, function (err, exe) {
+      t.ifError(err, 'no gen error')
 
-  throws('win-version-info requires a string filename, got: undefined')
-  throws('win-version-info requires a string filename, got: boolean', true)
+      t.same(vi(exe), xtend(dummyDefaults, {
+        LegalCopyright: '© Beep 嘟 Inc.'
+      }), 'info ok')
+    })
 
-  function throws(message, input) {
-    try {
-      vi(input)
-    } catch(err) {
-      t.is(err.message, message, 'throws')
+    gen({ assemblyCompany: '   © Beep 嘟 Inc.  ' }, function (err, exe) {
+      t.ifError(err, 'no gen error')
+
+      t.same(vi(exe), xtend(dummyDefaults, {
+        CompanyName: '© Beep 嘟 Inc.'
+      }), 'info ok')
+    })
+
+    gen({ assemblyTitle: '  嘟© Beep 嘟 Inc.嘟' }, function (err, exe) {
+      t.ifError(err, 'no gen error')
+
+      t.same(vi(exe), xtend(dummyDefaults, {
+        FileDescription: '嘟© Beep 嘟 Inc.嘟'
+      }), 'info ok')
+    })
+  })
+
+  test('throws on invalid input', function (t) {
+    t.plan(4)
+
+    throws('win-version-info requires a non-empty string filename', '')
+    throws('win-version-info is unable to access file: ' + process.cwd() + '\\nope.exe', 'nope.exe')
+
+    throws('win-version-info requires a string filename, got: undefined')
+    throws('win-version-info requires a string filename, got: boolean', true)
+
+    function throws(message, input) {
+      try {
+        vi(input)
+      } catch(err) {
+        t.is(err.message, message, 'throws')
+      }
     }
-  }
-})
+  })
 
-test('binding throws on non-string', function (t) {
-  var info = require('bindings')('VersionInfo')
+  test('binding throws on non-string', function (t) {
+    var info = require('bindings')('VersionInfo')
 
-  t.plan(1)
+    t.plan(1)
 
-  try {
-    info()
-  } catch(err) {
-    t.is(err.message, 'win-version-info requires a string filename', 'throws')
-  }
-})
+    try {
+      info()
+    } catch(err) {
+      t.is(err.message, 'win-version-info requires a string filename', 'throws')
+    }
+  })
+}
